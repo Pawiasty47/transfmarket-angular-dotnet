3c22f8e92ab6933900201576d914a8c8887751d9
Pawiasty47 <pawiasty47@gmail.com>
2026-04-07 16:09:05 +0200

    Poprwnie pusheniete pliki - Polaczenie warstwy .net z angularem, wstepne polaczenie modeli

---
+++ Angular17Client/Angular17Client/src/app/models/football.models.ts
@@ -0,0 +1,24 @@
+export interface Nationality {
+  id: number;
+  name: string;
+  trophies: number;
+}
+
+export interface Club {
+  id: number;
+  name: string;
+  foundationDate: string; 
+  trophies: number;
+}
+
+export interface Player {
+  id: number;
+  firstName: string;
+  lastName: string;
+  age: number;
+  weight: number;
+  clubId: number;
+  nationalityId: number;
+  club?: Club;               
+  nationality?: Nationality; 
+}
