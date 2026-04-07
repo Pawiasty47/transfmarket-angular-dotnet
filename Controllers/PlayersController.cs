a58008128c49732d355d7a2ffe7c40bfc573a50e
Pawiasty47 <pawiasty47@gmail.com>
2026-04-07 15:42:19 +0200

    Controler CRUD na modelu Player

---
+++ Controllers/PlayersController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Testx.Models;
+
+namespace Testx.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public PlayersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Player>>> GetPlayers()
+        {
+            return await _context.Players
+                .Include(p => p.Club)
+                .Include(p => p.Nationality)
+                .ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Player>> GetPlayer(int id)
+        {
+            var player = await _context.Players
+                .Include(p => p.Club)
+                .Include(p => p.Nationality)
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (player == null) return NotFound();
+
+            return player;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Player>> PostPlayer(Player player)
+        {
+            _context.Players.Add(player);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetPlayer), new { id = player.Id }, player);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPlayer(int id, Player player)
+        {
+            if (id != player.Id) return BadRequest();
+
+            _context.Entry(player).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Players.Any(e => e.Id == id)) return NotFound();
+                else throw;
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePlayer(int id)
+        {
+            var player = await _context.Players.FindAsync(id);
+            if (player == null) return NotFound();
+
+            _context.Players.Remove(player);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
