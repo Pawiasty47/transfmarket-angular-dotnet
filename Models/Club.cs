0be4e6ea0f638a70f3923056aa8828bf27a0b640
Pawiasty47 <pawiasty47@gmail.com>
2026-04-07 15:27:55 +0200

    Poprawienie modeli bazy danych

--- Models/Club.cs
+++ Models/Club.cs
@@ -1,6 +1,15 @@
-﻿namespace Testx.Models
+﻿using System.Numerics;
+
+namespace Testx.Models
 {
     public class Club
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime FoundationDate { get; set; }
+        public int Trophies { get; set; }
+
+        // Relacja: 1 Klub ma wielu piłkarzy
+        public ICollection<Player>? Players { get; set; }
     }
 }
--- Models/Nationality.cs
+++ Models/Nationality.cs
@@ -1,6 +1,14 @@
-﻿namespace Testx.Models
+﻿using System.Numerics;
+
+namespace Testx.Models
 {
     public class Nationality
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Trophies { get; set; } // Ilość trofeów narodowych
+
+        // Relacja: 1 Narodowość ma wielu piłkarzy
+        public ICollection<Player>? Players { get; set; }
     }
 }
