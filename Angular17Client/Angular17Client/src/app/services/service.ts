2dd8ddb9ea4d60784eff9c53ca9f17729a5f6640
Pawiasty47 <pawiasty47@gmail.com>
2026-04-07 16:07:30 +0200

    Polaczenie warstwy .net z angularem, wstepne polaczenie modeli

--- Angular17Client/Angular17Client/src/app/services/service.ts
+++ Angular17Client/Angular17Client/src/app/services/service.ts
@@ -1,6 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Player } from '../models/football.models';
+import { environment } from '../../enviroments/enviroments';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Service {}
+export class PlayerService {
+  private http = inject(HttpClient);
+  private apiUrl = `${environment.apiUrl}/api/Players`;
+
+  getPlayers(): Observable<Player[]> {
+    return this.http.get<Player[]>(this.apiUrl);
+  }
+
+
+  createPlayer(player: Player): Observable<Player> {
+    return this.http.post<Player>(this.apiUrl, player);
+  }
+
+
+  deletePlayer(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
